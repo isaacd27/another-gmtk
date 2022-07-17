0f1a3cfe8f002ef1ac4b8dcde02f83c43a1fda3c
Isaac Davis <72354474+isaacd27@users.noreply.github.com>
2022-07-17 09:54:18 -0400

    Working on enemy projectiles

--- Roll-n-Die/Assets/Scripts/[Msipp] To integrate/Coward.cs
+++ Roll-n-Die/Assets/Scripts/[Msipp] To integrate/Coward.cs
@@ -8,20 +8,23 @@ public class Coward : BasicEnemy
    
     public float cowardrad;
     public float rotationSpeed;
-    //public ffloat maxattack;
-    //float EnemyCool
+    //change this to whatever you want to name the enemy projectile.
+    public Projectile projPrefab;
+    
+    public float maxattack;
+    float EnemyCool;
     // Update is called once per frame
 
     public void Update()
     {
         Movement();
-            //EnemyCool -= time.deltatime;
+        EnemyCool -= Time.deltaTime;
     }
 
     protected override void Movement()
     {
 
-        
+
         float coward = Vector3.Distance(transform.position, Player.transform.position);
 
         if (coward <= cowardrad)
@@ -33,23 +36,27 @@ public class Coward : BasicEnemy
         }
         else
         {
-          // lookat the player
+            // lookat the player
 
-            /*  
+            transform.LookAt(Player.transform.position);
             if(EnemyCool <= 0f){
 
-            Projectile temp = GameObject.Instantiate(projPrefab, new Vector3(this.transform.position.x + d.x, this.transform.position.y + d.y), this.transform.rotation);
+            Projectile temp = GameObject.Instantiate(projPrefab, new Vector3(this.transform.position.x , this.transform.position.y), this.transform.rotation);
 
               temp.transform.position = this.transform.position + this.transform.up * 0.4f * Mathf.Sign(this.transform.localScale.x);
               // temp.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
 
 
-              temp.setDirection(d);
-            }*/
+              //temp.setDirection();
+            }
 
 
         }
 
 
+
+
     }
+
+
 }
