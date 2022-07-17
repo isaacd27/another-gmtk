710b1f79ac8e0c0bf2447efc97d75f47032b9139
Isaac Davis <72354474+isaacd27@users.noreply.github.com>
2022-07-16 21:46:16 -0400

    Add files via upload

--- Roll-n-Die/Assets/Scripts/[Msipp] To integrate/BasicEnemy.cs
+++ Roll-n-Die/Assets/Scripts/[Msipp] To integrate/BasicEnemy.cs
@@ -64,7 +64,9 @@ public class BasicEnemy : MonoBehaviour
     }
     protected void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Projectile"))
+
+        Projectile temp = collision.gameObject.GetComponent<Projectile>();
+        if (temp != null)
         {
             hp -= 1;
             
